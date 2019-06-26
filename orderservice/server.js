d75fdc31a35b6a9be1f12b8da2406f6dafb339e7
aakash1455 <reveal.aakash@gmail.com>
2019-06-26 13:55:14 +0530

    ecomm

--- orderservice/server.js
+++ orderservice/server.js
@@ -20,5 +20,5 @@ var server = app.listen(8087, function () {
   var host = server.address().address
   var port = server.address().port
   console.log("Example app listening at http://localhost:8087", host, port)
-  console.log("file updated")
+  
 })
